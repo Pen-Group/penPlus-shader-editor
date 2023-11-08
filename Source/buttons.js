572e2ff36e6b88f41d1962361b5a9a108ae23552
NexusKitten <127152751+NexusKitten@users.noreply.github.com>
2023-11-07 22:38:03 -0500

    Update buttons.js

--- Source/buttons.js
+++ Source/buttons.js
@@ -1,6 +1,8 @@
 {
     const glsl_Button = document.getElementById("ButtonGLSL")
     const blockly_Button = document.getElementById("ButtonBlockly")
+    const terminal_Button = document.getElementById("TerminalToggle")
+    const blockly = document.getElementById("BlocklyDiv")
 
     glsl_Button.onclick = () => {
         document.body.style.setProperty("--CodeVis", "visible");
@@ -16,5 +18,21 @@
 
         glsl_Button.className = "buttonUnselected";
         blockly_Button.className = "buttonSelected";
+    }
+
+    terminal_Button.onclick = () => {
+        if (blockly.style.width === "100%") {
+            document.body.style.setProperty("--TerminalVis", "visible");
+
+            blockly.style.width = "70%"
+            blockly.style.left = "30%"
+            Blockly.svgResize(workspace);
+        } else {
+            document.body.style.setProperty("--TerminalVis", "hidden");
+
+            blockly.style.width = "100%"
+            blockly.style.left = "0px"
+            Blockly.svgResize(workspace);
+        }
     }
 }
