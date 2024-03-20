140bc8d28742177e1d4a52d40322016f64778b5a
Obvious Alex C <76855369+David-Orangemoon@users.noreply.github.com>
2024-03-20 17:29:24 -0400

    Condense top bar even more.
    Move the settings and theme into the same dropdown under the gear icon which is to the right now.

--- Source/editor/buttons/general.js
+++ Source/editor/buttons/general.js
@@ -8,6 +8,9 @@
   const theme_Button = document.getElementById("DarkToggle");
   const fileButton = document.getElementById("fileButton");
 
+  const settingsDropdown = document.getElementById("settingsDropdown");
+  const settingsDropdownButton = document.getElementById("SettingsButton");
+
   //Secret button hidden within the options
   const recompileButton = document.getElementById("recompileButton");
   penPlus.recompileButton = recompileButton;
@@ -56,8 +59,12 @@
   theme_Button.onclick = () => {
     if (penPlus.editorTheme == "dark") {
       penPlus.editorTheme = "light";
+      themeIcon.innerHTML =
+        '<circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>';
     } else {
       penPlus.editorTheme = "dark";
+      themeIcon.innerHTML =
+        '<path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>';
     }
 
     localStorage.setItem("penPlusEditorTheme", penPlus.editorTheme);
@@ -68,14 +75,23 @@
   fileButton.onclick = () => {
     fileDropdown.style.visibility =
       fileDropdown.style.visibility == "hidden" ? "visible" : "hidden";
+    settingsDropdown.style.visibility = "hidden";
+  };
+
+  settingsDropdownButton.onclick = () => {
+    settingsDropdown.style.visibility =
+      settingsDropdown.style.visibility == "hidden" ? "visible" : "hidden";
+    fileDropdown.style.visibility = "hidden";
   };
 
   document.getElementById("prevAndConsole").onclick = () => {
     fileDropdown.style.visibility = "hidden";
+    settingsDropdown.style.visibility = "hidden";
   };
 
   blockly_div.onclick = () => {
     fileDropdown.style.visibility = "hidden";
+    settingsDropdown.style.visibility = "hidden";
   };
 
   document.getElementById("recompileButton").onclick = () => {
--- Source/editor/buttons/settings.js
+++ Source/editor/buttons/settings.js
@@ -1,4 +1,5 @@
 (function () {
+  const themeIcon = document.getElementById("themeIcon");
   const settingsButton = document.getElementById("OptionsButton");
 
   penPlus.editorTheme =
@@ -6,6 +7,14 @@
     (window.matchMedia("(prefers-color-scheme: dark)").matches
       ? "dark"
       : "light");
+
+  if (penPlus.editorTheme == "dark") {
+    themeIcon.innerHTML =
+      '<path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"></path>';
+  } else {
+    themeIcon.innerHTML =
+      '<circle cx="12" cy="12" r="5"></circle><line x1="12" y1="1" x2="12" y2="3"></line><line x1="12" y1="21" x2="12" y2="23"></line><line x1="4.22" y1="4.22" x2="5.64" y2="5.64"></line><line x1="18.36" y1="18.36" x2="19.78" y2="19.78"></line><line x1="1" y1="12" x2="3" y2="12"></line><line x1="21" y1="12" x2="23" y2="12"></line><line x1="4.22" y1="19.78" x2="5.64" y2="18.36"></line><line x1="18.36" y1="5.64" x2="19.78" y2="4.22"></line>';
+  }
 
   penPlus.autoCompile =
     localStorage.getItem("AutoCompile") === null
