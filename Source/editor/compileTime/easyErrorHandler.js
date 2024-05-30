383553f145ba7ef7d74ede54ba5bc94126c1659e
Obvious Alex C <76855369+David-Orangemoon@users.noreply.github.com>
2024-05-29 21:14:36 -0400

    Function returns

--- Source/editor/compileTime/easyErrorHandler.js
+++ Source/editor/compileTime/easyErrorHandler.js
@@ -91,6 +91,9 @@
         else if (error.includes("void function cannot return a value")) {
             errorElement.textContent = `Unexpected return : At line ${lineNumber} in GLSL`;
         }
+        else if (error.includes("function does not return a value")) {
+            errorElement.textContent = `function ${error.split(":")[2]} has is missing a returning point : At line ${lineNumber} in GLSL`;
+        }
 
         return errorElement;
     }
