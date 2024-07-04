1bc6a9b601c1907bf70f751ba50211a3f9ad6f3c
Obvious Alex C <76855369+David-Orangemoon@users.noreply.github.com>
2024-07-03 22:02:45 -0400

    Update variables PT:2

---
+++ Source/blockly/addons/rightClickVarDeletion.js
@@ -0,0 +1,34 @@
+(function() {
+    const deleteVar = {
+        id: 'deleteVar',
+        scopeType: Blockly.ContextMenuRegistry.ScopeType.BLOCK,
+        displayText: (scope) => {
+            console.log(scope);
+            return `Remove variable ${scope.block.variableData.mainText.split(" ")[1]}`
+        },
+        weight: 10,
+        preconditionFn: (scope) => {
+            if (scope.block.variableData) {
+                return 'enabled';
+            }
+            return 'hidden';
+        },
+        callback: (scope, e) => {
+            const variableName = scope.block.variableData.mainText;
+            penPlus.workspace.getAllBlocks().forEach(searchedBlock => {
+                if (searchedBlock.variableData && searchedBlock.variableData.mainText == variableName) {
+                    searchedBlock.dispose();
+                }
+            });
+
+            penPlus.workspace.getAllVariables().forEach(searchedVar => {
+                if (searchedVar.name == variableName) {
+                    penPlus.workspace.deleteVariableById(searchedVar.id_);
+                }
+            })
+        }
+        
+    }
+
+    Blockly.ContextMenuRegistry.registry.register(deleteVar);
+})();
