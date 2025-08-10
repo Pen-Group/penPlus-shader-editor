903f75c267311e737fe1425d02437bb95b46c1d0
Davey C II <71141838+AlexSchoolOH@users.noreply.github.com>
2025-08-10 12:57:33 -0400

    Fix all errors with the SVG exporter

--- Source/blockly/addons/rightClickSVGSave.js
+++ Source/blockly/addons/rightClickSVGSave.js
@@ -36,7 +36,7 @@
         callback: (scope, e) => {
             const group = scope.block.svgGroup_;
             const bounds = group.getBoundingClientRect();
-            const outerHTML = `<svg width="${Math.floor(bounds.width)}" height="${Math.floor(bounds.height)}" xmlns="http://www.w3.org/2000/svg" xmlns:html="http://www.w3.org/1999/xhtml" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1">${group.outerHTML.replaceAll("&nbsp;"," ").replaceAll(/class="[\w\d\s]*"/g, "").replace(/transform="[\w\.\(\)\s\d\,]*"/,"")}</svg>`;
+            const outerHTML = `<svg width="${Math.floor(bounds.width / penPlus.workspace.scale)}" height="${Math.floor(bounds.height / penPlus.workspace.scale)}" xmlns="http://www.w3.org/2000/svg" xmlns:html="http://www.w3.org/1999/xhtml" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1">${group.outerHTML.replaceAll("&nbsp;"," ").replaceAll(/class="[\w\d\s]*"/g, "").replace(/transform="[\w\.\(\)\s\d\,]*"/,"")}</svg>`;
             download(outerHTML,`${Date.now()}.svg`,"");
         }
         
